112c84e19c877995dfd452288d3dda28288d6c12
Kendra Steitz <kcsteitz@gmail.com>
2016-11-27 01:50:17 -0800

    Updated

--- Palindrome.py
+++ Palindrome.py
@@ -11,39 +11,38 @@
 #   1. Strip sentence of all whitespace
 #   2. Transfer to all lowercase
 #   3. Strip sentence of all punctuation
-#   4. Split sentence in half
-#   5. Reverse the second part of the sentence
-#   6. Compare the two halves
-#       if they are equal to each other, it's a palindrome
-#       if they are not equal, the sentence is not a palindrome
+#   4. Create function to find if sentence is a palindrome
+#       Using recursion
+#   5. Base case would be when sentence is less than 2 characters long
+#       Which would make the sentence a Palindrome
+#   6. Go through sentence and compare the first character against the
+#       last character.  If they are not the same, the sentence is not
+#       a palindrome.
+#   7. If the sentence length is greater than 2 and the first and last
+#       character are equal, call upon the palindrome function again
+#       using the sentence with the first and last characters cut off
 #
 
 from string import punctuation
 
 
 def palindrome(sentence):
-    s = sentence.replace(" ", "")
-    s = s.lower()
-    s = ''.join(c for c in s if c not in punctuation)
-    half = len(s) // 2
-    firstPart = s[:half]
-    secondPart = s[half:]
 
-    new = ""
-    i = 1
-    while i <= half:
-        new = new + (secondPart[len(secondPart) - i])
-        i = i + 1
-
-    if firstPart == new:
-        print(sentence, "is a Palindrome")
+    if len(sentence) < 2:
+        print("Sentence is a Palindrome.")
+    elif sentence[0] != sentence[-1]:
+        print("Sentence is not a Palindrome.")
     else:
-        print(sentence, "is not a Palindrome")
+        palindrome(sentence[1:-1])
 
 
 def main():
 
     sentence = input("Enter sentence: ")
-    palindrome(sentence)
+    s = sentence.replace(" ", "")
+    s = s.lower()
+    s = ''.join(c for c in s if c not in punctuation)
+
+    palindrome(s)
 
 main()
