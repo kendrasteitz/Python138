1191d5e097817d9a2543314b7a12e5a0ae4f54d9
Kendra Steitz <kcsteitz@gmail.com>
2016-12-16 01:33:28 -0800

    update

---
+++ employee.py
@@ -0,0 +1,16 @@
+# Exercise No.   3
+# File Name:     employee.py
+# Programmer:    Kendra Steitz
+# Date:          December 15, 2016
+#
+# Problem Statement:
+#
+#
+# Overall Plan:
+#   1.
+#
+
+from graphics import *
+from graphics import GraphWin, Point
+from Button import Button
+
